a86ff283907f6f2143732284c794e3e513d85154
Nawod <madhuvanthanawo1@gmail.com>
2021-09-20 23:56:16 +0530

    mobile responsive property added

--- src/blogCards.js
+++ src/blogCards.js
@@ -1,8 +1,10 @@
 import React from "react";
+// import './blogCard.css';
+import classes from './blogCard.module.css';
 
 const BlogCard = (props) => {
     return(
-        <div className="card">
+        <div className={classes.card}>
             <h3>{props.title}</h3>
             <p>{props.description}</p>
         </div>
