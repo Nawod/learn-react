a96b31e9f9889d81b724bf93577c042670032bfc
Nawod <madhuvanthanawo1@gmail.com>
2021-09-21 20:07:10 +0530

    add like button with count

--- src/App.js
+++ src/App.js
@@ -1,39 +1,77 @@
+import React from 'react';
 import './App.css';
 import BlogCard from './blogCards';
 
-function App() {
+class App extends React.Component{
+  state = {
+    showBlogs: true,
+    nObj : [
+      {
+        title: 'blog 1',
+        description: 'Testing react app Testing react app Testing react app',
+        likeCount : 0,
+      },
+      {
+        title: 'blog 2',
+        description: 'Testing react app Testing react app Testing react app',
+        likeCount : 0,
+      },
+      {
+        title: 'blog 3',
+        description: 'Testing react app Testing react app Testing react app',
+        likeCount : 0,
+      }
+    ]
+  }
 
-  const nObj = [
-    {
-      title: 'blog 1',
-      description: 'Testing react app Testing react app Testing react app'
-    },
-    {
-      title: 'blog 2',
-      description: 'Testing react app Testing react app Testing react app'
-    },
-    {
-      title: 'blog 3',
-      description: 'Testing react app Testing react app Testing react app'
-    }
-  ]
-  
-  const blogCards = nObj.map((item,pos)=>{
-    return(
-      <BlogCard key={pos} 
-        title={item.title} 
-        description={item.description}/>
-      // <div className="card" key={pos}>
-      //   <h3>Title: {item.title}</h3>
-      //   <p>Description: {item.description}</p>
-      // </div>
-    )
-  })
-  return ( 
-    <div className="App">
-      {blogCards}
-    </div>
-  );
+
+
+//like counts
+  likeClick = (position) =>{
+    const updatedBlogList = this.state.nObj;
+    const updatedBlogObj = updatedBlogList[position];
+
+    updatedBlogObj.likeCount = updatedBlogObj.likeCount +1;
+    updatedBlogList[position] = updatedBlogObj;
+
+    this.setState({nObj: updatedBlogList});
+
+  }
+ 
+
+//button onclick function
+  onHideButton = () => {
+    
+    this.setState((prevState, prevPrps) => {
+      return {showBlogs: !prevState.showBlogs}
+    });
+    
+  }
+
+//mentioned render components
+  render() {
+    //display blog cards
+    const blogCards = this.state.nObj.map((item,pos)=>{
+      return(
+        <BlogCard key={pos} 
+          title={item.title} 
+          description={item.description}
+          likeCount={item.likeCount}
+          likeClick={() => this.likeClick(pos)}
+        />
+      )
+    })
+    return ( 
+      <div className="App">
+        <div>
+          <button className="btn" onClick={this.onHideButton}>
+            {this.state.showBlogs ? 'Hide List' : 'Show List'}
+          </button>
+        </div>
+        {this.state.showBlogs ? blogCards : null}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/blogCards.js
+++ src/blogCards.js
@@ -3,12 +3,29 @@ import React from "react";
 import classes from './blogCard.module.css';
 
 const BlogCard = (props) => {
-    return(
-        <div className={classes.card}>
-            <h3>{props.title}</h3>
-            <p>{props.description}</p>
-        </div>
-    )
+
+    // const likeClick = () =>{
+    //     this.setState((prevState, prevProps) => {
+    //         return {
+    //             likeCount : prevState.likeCount + 1
+    //         }
+    //     });
+    // }
+
+
+
+        return(
+            <div className={classes.card}>
+                <h3>{props.title}</h3>
+                <p>{props.description}</p>
+                <p>Like Count : 
+                    <span className = {classes.LikeCount}>
+                        {props.likeCount}
+                    </span>
+                </p> 
+                <button onClick = {props.likeClick}>Like</button>
+            </div>
+        );
 }
 
 export default BlogCard;
